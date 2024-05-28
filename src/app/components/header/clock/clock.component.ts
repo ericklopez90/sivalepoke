6c4beffd54a90c92bb90dd4a70a26cdb234631e6
ericklopez90 <ericklopeza90@gmail.com>
2024-05-27 19:30:51 -0600

    clock v2 - add footer

--- src/app/components/header/clock/clock.component.ts
+++ src/app/components/header/clock/clock.component.ts
@@ -12,8 +12,14 @@ export class ClockComponent implements OnInit {
 
   ngOnInit() {
     this.time = interval(1000).pipe(
-      map(() => new Date().toLocaleTimeString())
-    );
-  }
-
+      map(() => new Date().toLocaleString('es-MX', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      })))
+    }
 }
